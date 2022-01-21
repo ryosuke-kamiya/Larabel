57c35c08d6579aa20ac32e1a89c9904127fc5a17
ryosuke-kamiya <cinesuija@gmail.com>
2022-01-21 15:50:05 +0900

    最終
    これ以降動画とはバージョンが違ったので進め方がわからず

---
+++ Bookmark/database/seeders/BookmarksTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class BookmarksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
